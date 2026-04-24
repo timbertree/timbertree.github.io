d2a9cf45fa58dc5f63b6e54b6125fb71c81cfb9e
timbertree <ruantianbo@gmail.com>
2026-04-24 11:37:25 +0800

    Site updated: 2026-04-24 11:37:25

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/03/24/init-1/","2026/03/24/init/","2026/03/24/1/","2026/03/24/hello-world/","2026/04/24/init2/"];function toRandomPost(){
+var posts=["2026/03/24/1/","2026/03/24/init/","2026/04/24/init2/","2026/03/24/hello-world/","2026/03/24/init-1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
